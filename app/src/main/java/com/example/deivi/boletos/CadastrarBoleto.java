9329415658c738351f9d1abbd8978b559dc46415
guilhermesilvacampos <34626129+guilhermesilvacampos@users.noreply.github.com>
2018-06-07 10:57:51 -0300

    CadastrarBoleto

---
+++ app/src/main/java/com/example/deivi/boletos/CadastrarBoleto.java
@@ -0,0 +1,13 @@
+package com.example.deivi.boletos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarBoleto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_boleto);
+    }
+}
