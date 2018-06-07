575eccae560fa55044bbf90d529acd42cfbab120
guilhermesilvacampos <34626129+guilhermesilvacampos@users.noreply.github.com>
2018-06-07 12:43:46 -0300

    Até_Selecionar_Imagem

--- app/src/main/java/com/example/deivi/boletos/CadastrarBoleto.java
+++ app/src/main/java/com/example/deivi/boletos/CadastrarBoleto.java
@@ -1,13 +1,91 @@
 package com.example.deivi.boletos;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CadastrarBoleto extends AppCompatActivity {
+        import android.Manifest;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.database.Cursor;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.net.Uri;
+        import android.provider.MediaStore;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v4.content.ContextCompat;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+
+public class CadastrarBoleto extends Activity {
+
+    ImageView imagem;
+    Button galeria;
+    private final int PERMISSAO_REQUEST = 2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_boleto);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!=  PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSAO_REQUEST);
+            }
+        }
+
+
+
+
+        imagem = (ImageView) findViewById(R.id.imagem);
+        galeria = findViewById(R.id.selecionar);
+
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPegaFoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentPegaFoto, 1);
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            Uri selectedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            Bitmap reduzido = Bitmap.createScaledBitmap(thumbnail,300,300,true);
+            imagem.setImageBitmap(reduzido);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode,String permissions[],  int[]  grantResults)  {
+        if(requestCode==  PERMISSAO_REQUEST)  {
+            if(grantResults.length>  0&&  grantResults[0]  ==  PackageManager.PERMISSION_GRANTED)  {
+
+            }  else{
+                //  A  permissão  foi  negada.  Precisa  ver  o  que  deve  ser  desabilitado}return;}}
+            }
+        }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/deivi/boletos/MainActivity.java
+++ app/src/main/java/com/example/deivi/boletos/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.deivi.boletos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +12,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onTelaCadastrar(View view){
+        Intent intent = new Intent(this,CadastrarBoleto.class);
+        startActivity(intent);
+    }
+
+
+    public void onTelaExcluir(View view){
+
+    }
+
+    public void onTelaCalendario(View view){
+
+    }
+
 }
