9a133a98903ec6b7f3690e39c5f71f7a44aea369
guilhermesilvacampos <34626129+guilhermesilvacampos@users.noreply.github.com>
2018-06-13 14:23:31 -0300

    notificacaoTecnicamentePronta

--- app/src/main/java/com/example/deivi/boletos/MainActivity.java
+++ app/src/main/java/com/example/deivi/boletos/MainActivity.java
@@ -11,9 +11,13 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Calendar;
+
+
 
 import model.VerificaBoletoVencido;
 
@@ -22,6 +26,7 @@ public class MainActivity extends Activity {
     Calendar calendar;
     NotificationManager mNotificationManager;
     PendingIntent tarefaPendingIntent;
+    AlarmManager alarmManager;
     Intent tarefaIntent;
     private int mId= 0;
 
@@ -33,31 +38,36 @@ public class MainActivity extends Activity {
 
 
 
+
         calendar = Calendar.getInstance();
 
         calendar.setTimeInMillis(System.currentTimeMillis());
 
-        calendar.set(Calendar.HOUR_OF_DAY, 6, 13, 11, 6, 10 );
+        calendar.set(Calendar.HOUR_OF_DAY, 6, 13, 14,24 , 10 );
 
         // With setInexactRepeating(), you have to use one of the AlarmManager interval
         // constants--in this case, AlarmManager.INTERVAL_DAY.
 
-
-
+        Log.i("1","BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+         alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
         tarefaIntent = new Intent(this, VerificaBoletoVencido.class);
         tarefaPendingIntent = PendingIntent.getBroadcast(this,0, tarefaIntent,0);
 
+        Log.i("1","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
 
-        AlarmManager alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
 
-//Definir o alarme para acontecer todos os dias às 10 horas
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, tarefaPendingIntent);
 
-
-
-
+        Log.i("1","PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+/*
+        if (alarmManager!= null) {
+            alarmManager.cancel(tarefaPendingIntent);
+            Log.i("1","Cancelouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu");
+        }
+*/
     }
 
 
--- app/src/main/java/model/VerificaBoletoVencido.java
+++ app/src/main/java/model/VerificaBoletoVencido.java
@@ -2,6 +2,7 @@ package model;
 
 import android.app.Activity;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -11,6 +12,10 @@ import android.content.Intent;
 import java.util.Calendar;
 import java.util.List;
 
+import android.graphics.BitmapFactory;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -26,7 +31,8 @@ import com.example.deivi.boletos.R;
 import dao.BoletoDAO;
 
 public class VerificaBoletoVencido extends BroadcastReceiver  {
-    MainActivity m = new MainActivity();
+
+
 Bundle bundle;
     private  int mId = 0;
 View view;
@@ -35,7 +41,7 @@ View view;
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.i ("1","TAAAAA VINDOOO" );
+        Log.i("1", "TAAAAA VINDOOO");
         BoletoDAO b = new BoletoDAO(context);
         Calendar c = Calendar.getInstance();
         int ano = c.get(Calendar.YEAR);
@@ -43,19 +49,45 @@ View view;
         int dia = c.get(Calendar.DAY_OF_MONTH);
 
         String data = dia + "/" + mes + "/" + ano;
-        Log.i ("1",data );
+        Log.i("1", data);
 
         List list = b.listBoletosVirouVencido(data);
 
-        Log.i ("1",list.toString () );
+        Log.i("1", list.toString());
 
         if (!(list == null)) {
 
-           Notificacao n = new Notificacao();
-           n.onCreate(bundle);
+            Log.i("Script", "-> Alarme");
 
-
+            gerarNotificacao(context, new Intent(context, MainActivity.class), "Nova mensagem", "Boleto em atraso", "Você tem um boleto em atraso!");
         }
+    }
+
+
+    public void gerarNotificacao(Context context, Intent intent, CharSequence ticker, CharSequence titulo, CharSequence descricao){
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent p = PendingIntent.getActivity(context, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setTicker(ticker);
+        builder.setContentTitle(titulo);
+        builder.setContentText(descricao);
+        builder.setSmallIcon(R.drawable.boleto);
+        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.boleto));
+        builder.setContentIntent(p);
+
+        Notification n = builder.build();
+        n.vibrate = new long[]{150, 300, 150, 600};
+        n.flags = Notification.FLAG_AUTO_CANCEL;
+        nm.notify(R.drawable.boleto, n);
+
+        try{
+            Uri som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone toque = RingtoneManager.getRingtone(context, som);
+            toque.play();
+        }
+        catch(Exception e){}
+    }
 
     }
 
@@ -90,4 +122,4 @@ View view;
     */
 
 
-}
+
